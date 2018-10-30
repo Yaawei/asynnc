e25c31037532c19f77897e85cf44f938398efeb4
Yawei <adamiakjawel@gmail.com>
2018-10-30 10:40:24 +0100

    Make the code cleaner

--- nnc/ext/memo.py
+++ nnc/ext/memo.py
@@ -16,80 +16,89 @@ class Memo(BaseModel):
     channel = peewee.CharField()
 
 
-@cmd('memo')
+@cmd("memo")
 async def save_memo(bot, msg):
-    contents = msg.params[-1].split(' ', 2)
-    if len(contents) == 3:
-        recipient = contents[1]
-        message = contents[2]
-        sender = msg.nick.rstrip('_')
-        sent_at = datetime.datetime.now()
-        channel = msg.channel
-
-        memo = await bot.objects.create(
-            Memo,
-            message=message,
-            recipient=recipient,
-            sender=sender,
-            sent_at=sent_at,
-            delivery_time=sent_at,
-            channel=channel,
-        )
-        bot.loop.call_soon(send_memo, bot, memo)
-        bot.reply(msg, 'successfully saved memo for %s' % recipient)
-
-    else:
+    contents = msg.params[-1].split(" ", 2)
+    if len(contents) != 3:
         bot.reply(
             msg,
-            'Wrong number of arguments. The correct format is - '
-            '!memo <recipient>(e.g. JohnDoe) <message>'
+            "Wrong number of arguments. The correct format is - "
+            "!memo <recipient>(e.g. JohnDoe) <message>",
         )
+        return
+
+    recipient = contents[1]
+    message = contents[2]
+    sender = msg.nick.rstrip("_")
+    sent_at = datetime.datetime.now()
+    channel = msg.channel
+
+    memo = await bot.objects.create(
+        Memo,
+        message=message,
+        recipient=recipient,
+        sender=sender,
+        sent_at=sent_at,
+        delivery_time=sent_at,
+        channel=channel,
+    )
+    bot.loop.call_soon(send_memo, bot, memo)
+    bot.reply(msg, "Successfully saved memo for %s" % recipient)
 
 
-@cmd('remind')
+@cmd("remind")
 async def save_remind(bot, msg):
-    contents = msg.params[-1].split(' ', 3)
-    if len(contents) == 4:
-        recipient = contents[1]
-        message = contents[3]
-        delivery_time, delivery_time_delta = parse_time(contents[2])
-        sender = msg.nick.rstrip('_')
-        sent_at = datetime.datetime.now()
-        channel = msg.channel
-
-        memo = await bot.objects.create(
-            Memo,
-            message=message,
-            recipient=recipient,
-            sender=sender,
-            sent_at=sent_at,
-            delivery_time=delivery_time,
-            channel=channel,
-        )
-        bot.loop.call_later(delivery_time_delta.total_seconds(), send_memo, bot, memo)
-        bot.reply(msg, 'reminder for %s set at %s' % (recipient, str(delivery_time)[:-7]))
-
-    else:
+    contents = msg.params[-1].split(" ", 3)
+    if len(contents) != 4:
         bot.reply(
             msg,
-            'Wrong number of arguments. The correct format is - '
-            '!remind <recipient>(e.g. JohnDoe) <time>(e.g. 2d,4h,15m,35s) <message>'
+            "Wrong number of arguments. The correct format is - "
+            "!remind <recipient>(e.g. JohnDoe) <time>(e.g. 2d,4h,15m,35s) <message>",
         )
+        return
+
+    recipient = contents[1]
+    message = contents[3]
+    delivery_time, delivery_time_delta = parse_time(contents[2])
+    sender = msg.nick.rstrip("_")
+    sent_at = datetime.datetime.now()
+    channel = msg.channel
+
+    memo = await bot.objects.create(
+        Memo,
+        message=message,
+        recipient=recipient,
+        sender=sender,
+        sent_at=sent_at,
+        delivery_time=delivery_time,
+        channel=channel,
+    )
+    bot.loop.call_later(delivery_time_delta.total_seconds(), send_memo, bot, memo)
+    bot.reply(
+        msg,
+        "Reminder for %s set at %s"
+        % (recipient, delivery_time.strftime("%H:%M:%S, %e %b %Y")),
+    )
 
 
 def send_memo(bot, memo):
     stripped_users = [user.lower().strip("@_") for user in bot.channels[memo.channel]]
-    if memo.recipient.lower().strip("@_") not in stripped_users \
-            or memo.delivery_time > datetime.datetime.now():
+    if (
+        memo.recipient.lower().strip("@_") not in stripped_users
+        or memo.delivery_time > datetime.datetime.now()
+    ):
         bot.loop.call_later(60, send_memo, bot, memo)
     else:
-        bot.say(memo.channel,
-                '%s (to %s, from %s on %s)' % (
-                    memo.message,
-                    memo.recipient,
-                    memo.sender,
-                    memo.sent_at.strftime("%H:%M, %e %b %Y"),
-                 ))
+        bot.say(
+            memo.channel,
+            "%s (to %s, from %s on %s)"
+            % (
+                memo.message,
+                memo.recipient,
+                memo.sender,
+                memo.sent_at.strftime("%H:%M, %e %b %Y"),
+            ),
+        )
         asyncio.create_task(bot.objects.delete(memo))
 
 
@@ -101,17 +110,21 @@ async def load_memo_from_db(bot):
 
 
 def parse_time(time_str):
-    legal_letters = ['h', 'd', 's', 'm']
-    time_split = time_str.split(',')
-    times = {x[-1]: int(x[:-1]) for x in time_split
-             if x and x[-1] in legal_letters and x[:-1].isdigit()}
+    legal_letters = ["h", "d", "s", "m"]
+    time_split = time_str.split(",")
+    times = {
+        x[-1]: int(x[:-1])
+        for x in time_split
+        if x and x[-1] in legal_letters and x[:-1].isdigit()
+    }
     if times:
         for let in legal_letters:
             if let not in times.keys():
                 times[let] = 0
 
         delivery_time_delta = datetime.timedelta(
-            days=times['d'], hours=times['h'], minutes=times['m'], seconds=times['s'])
+            days=times["d"], hours=times["h"], minutes=times["m"], seconds=times["s"]
+        )
         delivery_time = datetime.datetime.now() + delivery_time_delta
 
         return delivery_time, delivery_time_delta
